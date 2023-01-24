2696aaf248ef9530899e8db33ec41388b0732594
EnriqueFlores5 <123483066+EnriqueFlores5@users.noreply.github.com>
2023-01-24 09:27:47 -0600

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,2 +1,20 @@
 // Don't forget to comment your code as you work!
 scene.setBackgroundColor(7)
+let soccerPlayer = sprites.create(img`
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . f f f f f . . . . . 
+    . . . . f f f f f f f f . . . . 
+    . . . f f f f f f f f f f f . . 
+    . . f f f f 4 4 4 4 f f f f . . 
+    . f f 4 f b f 4 4 f b f 4 f f . 
+    . f 4 4 4 1 f 4 4 f 1 4 4 4 f . 
+    . . f 4 4 4 4 4 4 4 4 4 4 f . . 
+    . . . f 4 4 f f f f 4 4 f . . . 
+    . . 4 4 8 9 9 9 9 9 9 8 4 4 . . 
+    . . 4 4 8 9 8 9 9 9 9 8 4 4 . . 
+    . . 4 4 8 9 9 9 9 9 9 8 4 4 . . 
+    . . . . . 1 1 1 1 1 1 . . . . . 
+    . . . . . 1 1 . . 1 1 . . . . . 
+    `, SpriteKind.Player)
