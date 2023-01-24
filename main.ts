e3ec18f3022dfa19c553ceea34914125ca517d14
EnriqueFlores5 <123483066+EnriqueFlores5@users.noreply.github.com>
2023-01-24 09:37:08 -0600

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,4 +1,8 @@
+controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
+    soccerBall.throwDart()
+})
 // Don't forget to comment your code as you work!
+let soccerBall: Dart = null
 scene.setBackgroundColor(7)
 let soccerPlayer = sprites.create(img`
     . . . . . . . . . . . . . . . . 
@@ -18,3 +22,25 @@ let soccerPlayer = sprites.create(img`
     . . . . . 1 1 1 1 1 1 . . . . . 
     . . . . . 1 1 . . 1 1 . . . . . 
     `, SpriteKind.Player)
+soccerPlayer.setPosition(75, 103)
+soccerBall = darts.create(img`
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . 1 1 1 1 . . . . . . 
+    . . . 1 1 1 1 f f 1 1 1 1 . . . 
+    . . 1 1 1 1 1 1 1 1 1 f 1 1 . . 
+    . 1 f f f 1 f f f 1 1 1 1 1 . . 
+    . 1 f f f f 1 f 1 1 1 f 1 1 1 1 
+    . . 1 f f f f 1 f f f f 1 1 f 1 
+    . . 1 1 f 1 1 1 1 f f f 1 f f 1 
+    . . . 1 f f 1 f 1 1 f f 1 f f 1 
+    . . . 1 1 1 1 f f 1 1 f 1 f 1 1 
+    . . . . 1 1 1 1 1 1 1 1 1 1 . . 
+    . . . . . 1 1 1 1 1 1 1 1 1 . . 
+    . . . . . . . . 1 . . . . . . . 
+    `, SpriteKind.Projectile)
+soccerBall.setPosition(75, 78)
+soccerBall.setTrace()
+soccerBall.controlWithArrowKeys()
